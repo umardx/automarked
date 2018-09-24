b89167e33963c7e0a25a4182e2aa4c67f1e83606
Umar Nawawi <umardx@s.itb.ac.id>
2018-09-24 20:06:55 +0700

    modified views, remote css & js

--- automarked/views.py
+++ automarked/views.py
@@ -1,11 +1,11 @@
-import os
-from flask import render_template, redirect, url_for, request
+from os import environ
+from flask import render_template, redirect, url_for, request, flash
 from automarked import app, db, hs
 from automarked.models import LoginForm, SignupForm, User
 
 def getAppName():
     try:
-        app_name = str(os.environ.get('APP_NAME'))
+        app_name = str(environ.get('APP_NAME'))
     except:
         app_name = 'Automarked'
     return app_name
@@ -14,19 +14,24 @@ def getAppName():
 def index():
     app_name = getAppName()
     title = 'Home — ' + app_name
+    sign_err = None
+    sign_msg = None
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
             user = User.query.filter_by(username=form.username.data).first()
             if user and hs.check_password_hash(user.password, form.password.data):
-                return redirect(url_for('dashboard'))
-            return "<h1>Invalid Username or Password!</h1>"
-    return render_template('index.html', form=form, title=title, app_name=app_name)
+                sign_msg = 'You were successfully logged in'
+            else:
+                sign_err = 'Get the fuck out here!'
+    return render_template('index.html', form=form, title=title, app_name=app_name, sign_msg=sign_msg, sign_err=sign_err)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     app_name = getAppName()
     title = 'Signup — ' + app_name
+    sign_err = None
+    sign_msg = None
     form = SignupForm()
 
     if form.validate_on_submit():
@@ -37,12 +42,17 @@ def signup():
         )
         db.session.add(new_user)
         db.session.commit()
-        return '<h1>Account has been created!</h1>'
+        sign_msg = 'Account has been created!'
 
-    return render_template('signup.html',form=form, title=title, app_name=app_name)
+    return render_template('signup.html',form=form, title=title, app_name=app_name, sign_msg=sign_msg, sign_err=sign_err)
 
 @app.route('/dashboard')
 def dashboard():
     app_name = getAppName()
     title = 'Dashboard — ' + app_name
     return render_template('dashboard.html', title=title, app_name=app_name)
+
+# TODO
+# [] register unique username or email
+# [] flask session
+# [] flask security 
