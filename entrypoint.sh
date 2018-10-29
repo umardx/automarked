2227a56d500c86a1560de60568ed8770cf2f654e
Umar Nawawi <12740465+umardx@users.noreply.github.com>
2018-10-29 20:55:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 echo '[flask db init...]'
-if [ ! -d "database" ]; then
 pipenv run flask db init
-fi
 echo '[flask db migrate...]'
 pipenv run flask db migrate
 echo '[flask db upgrade...]'
