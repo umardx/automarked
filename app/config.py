9d88b3a2cff765d80ba142ec8dafa4019ad2581f
Umar Nawawi <umardx@s.itb.ac.id>
2019-02-04 14:13:12 +0700

    config ok

--- app/config.py
+++ app/config.py
@@ -12,22 +12,9 @@ def get_db_file():
     _db_file = 'sqlite:///{}'.format(path.join(_db_dir, 'app.db'))
     return _db_file
 
-def get_celery_file():
-    """
-    getCeleryFile() define database's file path to
-    `./automarked/database/celery.db`
-    """
-    _db_dir = path.join(Path(path.dirname(__file__)).parent, 'database')
-    Path(_db_dir).mkdir(parents=True, exist_ok=True)
-    _db_file = 'sqlite:///{}'.format(path.join(_db_dir, 'celery.db'))
-    return _db_file
 
-
-BROKER_TRANSPORT = "sqlalchemy"
-BROKER_HOST = get_celery_file()
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 SECRET_KEY = 'AAAAAH/2wCEAAwICAgJCAwJCQwRCwoLERUPDAwPFRgTExUTExgRDAwM'
 RECAPTCHA_PUBLIC_KEY = '6LcnP44UAAAAAFCNi_E2F7DH8ItMEEtEWbLQHmN1'
